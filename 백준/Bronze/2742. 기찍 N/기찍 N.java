da435f3e39042081d5b95ff9a211475c69a162df
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-05 21:24:56 +0900

    [Bronze IV] Title: 기찍 N, Time: 1200 ms, Memory: 32980 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String [] arg) {
+		  Scanner s = new Scanner(System.in);
+	      int a = s.nextInt();
+	      for (int i = a; i >= 1; i--) {
+			System.out.println(i);
+	      }
+    }
+}
