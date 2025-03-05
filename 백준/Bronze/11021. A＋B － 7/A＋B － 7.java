0891ffc081f3aeab93802e906b0d56efd7e45405
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-05 21:29:01 +0900

    [Bronze V] Title: A+B - 7, Time: 264 ms, Memory: 18692 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {	
+	public static void main(String[] args){
+	    Scanner s = new Scanner(System.in);
+		int count = s.nextInt();
+		int a;
+		int b;
+		int[] answer = new int[count];
+		for (int i = 0; i < count; i++) {
+			a = s.nextInt();
+			b = s.nextInt();
+			answer[i] = a+b;
+		}
+		a = 1;
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println("Case #"+a+": "+answer[i]);
+			a++;
+		}
+    }
+}
