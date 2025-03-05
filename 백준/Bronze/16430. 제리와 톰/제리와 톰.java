1f4970031d9958d918e088f55fb69ccdca10cf5f
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-05 21:32:45 +0900

    [Bronze V] Title: 제리와 톰, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+public class Main {
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		a = b - a;
+		System.out.println(a+" "+b);
+	}
+}
