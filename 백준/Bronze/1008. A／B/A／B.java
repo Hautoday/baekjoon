d29fcbf4bfc9408df3ab50e57e02da0e9980cae6
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-05 21:23:09 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String [] arg) {
+		Scanner s = new Scanner(System.in);
+		double A = s.nextDouble();
+		double B = s.nextDouble();
+		System.out.println(A/B);
+
+	}
+
+}
+
