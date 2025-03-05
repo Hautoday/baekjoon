853f3c9faa9abc4c3e08fdec7c6ef450f9959cf6
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-05 21:24:52 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String [] arg) {
+		Scanner s = new Scanner(System.in);
+		int A = s.nextInt();
+		int B = s.nextInt();
+		System.out.println(A*B);
+
+	}
+
+}
+
