8963a2e952afd5d4afa78aa6f019425d50ef7afe
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-16 19:52:19 +0900

    [Silver II] Title: N과 M (9), Time: 184 ms, Memory: 21192 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int M,N;
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr, answer;
+    static boolean[] state;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+        answer = new int[N];
+        state = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+
+        System.out.print(sb.toString());
+    }
+
+    public static void dfs(int index) {
+        if (index == M){
+            for (int i = 0; i < M; i++){
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int save = 0;
+        for (int i = 0; i < N; i++) {
+            if(state[i]){
+                continue;
+            }
+            if(save != arr[i]){
+                state[i] = true;
+                answer[index] = arr[i];
+                save = arr[i];
+                dfs(index + 1);
+                state[i] = false;
+            }
+        }
+    }
+
+}
