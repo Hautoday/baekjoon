132c4277f86e3326e099394b70f508b449fbb829
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-03-24 20:40:44 +0900

    [level 2] Title: 구명보트, Time: 12.41 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int count = 0;
+        for(int i = people.length-1; i >= count; i--){
+            if(people[i] + people[count] <= limit){
+                count++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
