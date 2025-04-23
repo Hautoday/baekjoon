274b661542f05b3de93dceb94a95734b4afc8edf
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-04-23 22:09:17 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.14 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for (int i = 1; i <= count; i++){
+            answer += price * i ;
+        }
+        if( money > answer){
+            return 0;
+        }else{
+            answer = answer - money;   
+        }
+        return answer;
+    }
+}
