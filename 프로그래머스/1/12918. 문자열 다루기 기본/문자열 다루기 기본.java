317f07eb9a2994cc4ab085ce69797ad2d671ab49
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-08-21 14:39:35 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.10 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() == 4 || s.length() == 6){
+            try{
+               int a = Integer.parseInt(s); 
+            }catch(Exception e){
+                return false;
+            }
+        }else{
+            return false;
+        }
+        return answer;
+    }
+}
