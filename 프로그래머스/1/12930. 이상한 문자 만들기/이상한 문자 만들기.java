94285e480fb685aaca6ab433cd0a1f04845553d7
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-08-26 20:57:37 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.62 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split(" ");
+        int index = 0;
+        for(int i = 0; i < str.length; i++){
+            if(str[i].equals(" ")){
+                index = 1;
+            }
+            if(index % 2 == 0){
+                answer += str[i].toUpperCase();
+            }else{
+                answer += str[i].toLowerCase();
+            }
+            index++;
+        }
+        return answer;
+    }
+}
