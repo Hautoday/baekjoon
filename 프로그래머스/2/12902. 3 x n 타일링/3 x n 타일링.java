d364e84aea389b01c1964bd120f70a13460b3418
Hautoday <42128097+Hautoday@users.noreply.github.com>
2025-10-16 15:57:01 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.36 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -1,18 +1,17 @@
 class Solution {
     public int solution(int n) {
-        final int maxnum = 1000000007;
-        int[] dp = new int[5000];
+        final int max = 1000000007;
+        int[] dp = new int[5001];
         dp[1] = 2;
         dp[2] = 3;
-        dp[4] = 11;
-        for(int i = 3; i <= n; i++){
-            if(i % 2 == 0){
-                dp[i] = ((dp[i-1] % maxnum) + (dp[i-2] % maxnum)) % maxnum;
+        dp[3] = 8;
+        for(int i = 4; i <= n; i++){
+            if( i % 2 == 0){
+                dp[i] = ((dp[i-1] % max) + (dp[i-2] % max)) % max;
             }else{
-                dp[i] = ((dp[i - 1] * 2) % maxnum) + ((dp[i - 2]) % maxnum) % maxnum;
+                dp[i] = (((dp[i-1] * 2) % max) + (dp[i-2] % max)) % max;
             }
         }
         return dp[n];
     }
 }
-
